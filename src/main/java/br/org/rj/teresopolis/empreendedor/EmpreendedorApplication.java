f0601d5a4f63d6a97f8e0c8fd100d7ce7e792c6c
RaphFernandes <rf10@globo.com>
2020-11-09 02:11:52 -0300

    Project created

---
+++ src/main/java/br/org/rj/teresopolis/empreendedor/EmpreendedorApplication.java
@@ -0,0 +1,13 @@
+package br.org.rj.teresopolis.empreendedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpreendedorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpreendedorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/rj/teresopolis/empreendedor/EmpreendedorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.rj.teresopolis.empreendedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpreendedorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
